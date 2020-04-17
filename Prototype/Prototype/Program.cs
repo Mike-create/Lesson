204527dcfad6fcc32e5774be896b5b444285846c
Mike-create <63596079+Mike-create@users.noreply.github.com>
2020-04-17 15:30:15 +0300

    Add new file Prototype

--- Prototype/Prototype/Program.cs
+++ Prototype/Prototype/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 
-namespace FirstRepo
+namespace Prototype
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
     }
